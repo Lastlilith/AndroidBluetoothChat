96e8fa795485c261b41592b963671abc9e9aca5b
Lastlilith <lastlilith.2014@gmail.com>
2023-04-24 14:46:29 +0900

    Initial commit

---
+++ app/src/main/java/com/imnidasoftware/androidbluetoothchat/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.imnidasoftware.androidbluetoothchat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.imnidasoftware.androidbluetoothchat.ui.theme.AndroidBluetoothChatTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidBluetoothChatTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndroidBluetoothChatTheme {
+        Greeting("Android")
+    }
+}
